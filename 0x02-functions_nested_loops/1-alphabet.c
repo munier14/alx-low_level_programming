5f906f18c657c3ba41e4e2890f9278e4b1397e87
Munier Hussien <munier4@gmail.com>
2021-09-01 13:34:28 +0000

    kill

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
 #include "holberton.h"
-#include "main.h"
+
 /**
- * print_alphabet - prints alphabet
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
  */
 void print_alphabet(void)
 {
-	char c;
+	int n;
 
-	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
 	_putchar('\n');
 }
