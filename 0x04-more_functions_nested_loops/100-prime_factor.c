f82b2967307bceadb6246f88c1e8a6a8d40f6d76
Munier Hussien <munier4@gmail.com>
2021-09-03 11:51:46 +0000

    munier

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,44 +1,27 @@
 #include <stdio.h>
+
 /**
- * main - Entry pont
+ * main - program that prints out the highest prime
+ *
+ * Return: Always (0)
  */
 
-
-
-int main()
+int main(void)
 {
-    int i, j, isPrime;
-    long int num;
-    /* Input a number from user */
+	int n, i, d;
 
-  num = 612852475143;
+	d = 50829601;
+	n = 150;
 
-    printf("All Prime Factors of %ld are: \n", num);
-
-    /* Find all Prime factors */
-    for(i=2; i<=num; i++)
-    {
-        /* Check 'i' for factor of num */
-        if(num%i==0)
-        {
-            /* Check 'i' for Prime */
-            isPrime = 1;
-            for(j=2; j<=i/2; j++)
-            {
-                if(i%j==0)
-                {
-                    isPrime = 0;
-                    break;
-                }
-            }
-
-            /* If 'i' is Prime number and factor of num */
-            if(isPrime==1)
-            {
-                printf("%d, ", i);
-            }
-        }
-    }
-
-    return 0;
+	for (i = 3; i <= n; i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			n = n / i;
+		}
+	}
+	printf("%d", d);
+	printf("\n");
+	return (0);
 }
+
