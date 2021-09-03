adbb7bdd4b1394c39287e552960981d62ca03a28
Munier Hussien <munier4@gmail.com>
2021-09-03 08:32:31 +0000

    good

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code for alx School students.
+ * Return: void .
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar (i + '0');
+}
+_putchar('\n');
+}
+© 2021 GitHub, Inc.
